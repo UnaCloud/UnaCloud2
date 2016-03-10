73dc5c3df0cd136dd51f364528586fa60b5c25ae
CesarF <cesar.forero.gz@gmail.com>
2016-03-10 18:49:52 -0500

    Documentation for enums

--- UnaCloudWeb/grails-app/domain/unacloud/enums/IPEnum.java
+++ UnaCloudWeb/grails-app/domain/unacloud/enums/IPEnum.java
@@ -1,7 +1,26 @@
 package unacloud.enums;
 
+/**
+ * Represents IP states
+ * @author CesarF
+ *
+ */
 public enum IPEnum {
-	
-	USED,RESERVED,DISABLED,AVAILABLE;
+	/**
+	 * IP is assign a current deployed execution
+	 */
+	USED,
+	/**
+	 * IP has been assigned to execution
+	 */
+	RESERVED,
+	/**
+	 * Disabled by admin used
+	 */
+	DISABLED,
+	/**
+	 * IP is available to be assign
+	 */
+	AVAILABLE;
 
 }
