b8bb3b748998d79fc6537c150335ba832f8ec13e
CesarF <cesar.forero.gz@gmail.com>
2016-04-05 13:21:35 -0500

    Scripts for install and deploy in Ubunto/Debian

---
+++ scripts/install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+export PATH_CONFIG=""
+RABBIT_USER=$(grep -w "QUEUE_USER" ${PATH_CONFIG}config.properties  | cut -c12-)
+RABBIT_USER=`echo $RABBIT_USER | tr -d '\r'`
+RABBIT_PASS=$(grep -w "QUEUE_PASS" ${PATH_CONFIG}config.properties  | cut -c12-)
+RABBIT_PASS=`echo $RABBIT_PASS | tr -d '\r'`
+
+apt-get update
+
+#Rabbit
+apt-get install rabbitmq-server
+/usr/lib/rabbitmq/bin/rabbitmq-plugins enable rabbitmq_management
+rabbitmqctl add_user $RABBIT_USER $RABBIT_PASS
+rabbitmqctl set_user_tags $RABBIT_USER administrator
+rabbitmqctl set_permissions -p / $RABBIT_USER ".*" ".*" ".*"
+rabbitmqctl delete_user guest
+rabbitmqctl stop
+invoke-rc.d rabbitmq-server start
+
+#Tomcat
+apt-get update
+apt-get install openjdk-7-jdk
+apt-get install default-jdk
+mkdir /opt/tomcat
+wget http://www-us.apache.org/dist/tomcat/tomcat-8/v8.0.33/bin/apache-tomcat-8.0.33.tar.gz
+tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1
+
+#UnaCloud
+mkdir /opt/unacloud
+mkdir /opt/unacloud/repo
+export UNACLOUD_HOME=/opt/unacloud
+unset RABBIT_USER
+unset RABBIT_PASS
+
+cp -f ${PATH_CONFIG}UnaCloud.war $CATALINA_HOME/webapps/
+cp -f ${PATH_CONFIG}FileManager.war $CATALINA_HOME/webapps/
+cp -f ${PATH_CONFIG}CloudControl.jar $UNACLOUD_HOME/
