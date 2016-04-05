7589785b262ff8c20a8eaf163c603df439a188a1
CesarF <cesar.forero.gz@gmail.com>
2016-04-05 18:03:50 -0500

    refactor installer

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,12 @@ RABBIT_USER=$(grep -w "QUEUE_USER" ${PATH_CONFIG}config.properties  | cut -c12-)
 RABBIT_USER=`echo $RABBIT_USER | tr -d '\r'`
 RABBIT_PASS=$(grep -w "QUEUE_PASS" ${PATH_CONFIG}config.properties  | cut -c12-)
 RABBIT_PASS=`echo $RABBIT_PASS | tr -d '\r'`
+MYSQL_USER=$(grep -w "DB_USERNAME" ${PATH_CONFIG}config.properties  | cut -c12-)
+MYSQL_USER=`echo $MYSQL_USER | tr -d '\r'`
+MYSQL_PASS=$(grep -w "DB_PASS" ${PATH_CONFIG}config.properties  | cut -c12-)
+MYSQL_PASS=`echo $MYSQL_PASS | tr -d '\r'`
+MYSQL_DB=$(grep -w "DB_NAME" ${PATH_CONFIG}config.properties  | cut -c12-)
+MYSQL_DB=`echo $MYSQL_DB | tr -d '\r'`
 
 apt-get update
 
@@ -25,13 +31,23 @@ mkdir /opt/tomcat
 wget http://www-us.apache.org/dist/tomcat/tomcat-8/v8.0.33/bin/apache-tomcat-8.0.33.tar.gz
 tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1
 
+#mysql
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password '$MYSQL_PASS
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password '$MYSQL_PASS
+apt-get install mysql-server
+mysql --user=$MYSQL_USER --password=$MYSQL_PASS --execute="CREATE DATABASE ${MYSQL_DB};"
+
 #UnaCloud
 mkdir /opt/unacloud
 mkdir /opt/unacloud/repo
 export UNACLOUD_HOME=/opt/unacloud
 unset RABBIT_USER
 unset RABBIT_PASS
+unset MYSQL_USER
+unset MYSQL_PASS
+unset MYSQL_DB
 
 cp -f ${PATH_CONFIG}UnaCloud.war $CATALINA_HOME/webapps/
 cp -f ${PATH_CONFIG}FileManager.war $CATALINA_HOME/webapps/
 cp -f ${PATH_CONFIG}CloudControl.jar $UNACLOUD_HOME/
+
