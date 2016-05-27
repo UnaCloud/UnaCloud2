e53fa14b25fbd06ad9c37a233c7c73827eaf19e2
CesarF <cesar.forero.gz@gmail.com>
2016-05-27 17:23:40 -0500

    Added new IP Service and test files

---
+++ UnaCloudWeb/grails-app/services/unacloud/allocation/IpAllocatorService.groovy
@@ -0,0 +1,59 @@
+package unacloud.allocation
+
+import com.losandes.enums.VirtualMachineExecutionStateEnum;
+
+import grails.transaction.Transactional
+import unacloud.DeployedImage
+import unacloud.ExecutionIP
+import unacloud.NetInterface
+import unacloud.VirtualMachineExecution
+import unacloud.pmallocators.AllocatorException
+import unacloud.share.enums.IPEnum;
+
+/**
+ * This service is only for process.
+ * Service used to determinate IP allocation for virtual executions.
+ * This class should be a service to use hibernate connection.
+ * @author CesarF
+ *
+ */
+@Transactional
+class IpAllocatorService {
+
+    //-----------------------------------------------------------------
+	// Methods
+	//-----------------------------------------------------------------
+	
+	/**
+	 * Allocates IP addresses to all virtual machines in parameters
+	 * @param list of virtual machines executions
+	**/
+	//TODO manage net interfaces configuration	
+	def allocateIPAddresses(virtualExecutions){		
+		for(VirtualMachineExecution vme in virtualExecutions){
+			if(vme.status.equals(VirtualMachineExecutionStateEnum.QUEUED)){
+				List <ExecutionIP> ips= vme.executionNode.laboratory.getAvailableIps()
+				for(ip in ips){
+					if(ip.state==IPEnum.AVAILABLE){
+						NetInterface netInterface = new NetInterface(name:'eth0',ip:ip,virtualExecution:vme)
+						vme.interfaces.add(netInterface)
+						ip.putAt('state',IPEnum.RESERVED)
+						String[] subname= ip.ip.split("\\.")
+						vme.setName(vme.name+subname[2]+subname[3]) 
+						break
+					}
+				}
+				if (vme.interfaces.size()==0){ 
+					for(VirtualMachineExecution vm in virtualExecutions){
+						if(vme.status.equals(VirtualMachineExecutionStateEnum.QUEUED)){
+							for(NetInterface net in vme.interfaces){
+								net.ip.putAt('state',IPEnum.AVAILABLE)
+							}
+						}						
+					}
+					throw new AllocatorException("Not enough IPs for this deployment")
+				}
+			}
+		}
+	}		
+}
