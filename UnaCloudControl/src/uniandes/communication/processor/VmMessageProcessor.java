8d60594006e2579cf6301c12a610c0d6582425a8
CesarF <cesar.forero.gz@gmail.com>
2016-06-02 16:10:12 -0500

    comment messages in code

--- UnaCloudControl/src/uniandes/communication/processor/VmMessageProcessor.java
+++ UnaCloudControl/src/uniandes/communication/processor/VmMessageProcessor.java
@@ -33,7 +33,7 @@ public class VmMessageProcessor extends AbstractReceiverProcessor{
 			jsonMessage = jsonMessage.getJSONObject("data");
 			if(message.getType().equals(UDPMessageEnum.STATE_VM)){
 				try(Connection con = ControlManager.getInstance().getDBConnection();){
-					System.out.println(jsonMessage.toString());
+					//System.out.println(jsonMessage.toString());
 					//UDPMessageEnum.STATE_VM, "hostname",OperatingSystem.getHostname(),"executionId",virtualMachineCode,"state",state.toString(),"message",message
 					System.out.println("Report VM: "+message.getHost()+" - "+jsonMessage.get("hostname"));
 					PhysicalMachineUpdater.updateVirtualExecution(jsonMessage.getLong("executionId"),jsonMessage.getString("hostname"), jsonMessage.getString("message"), VirtualMachineExecutionStateEnum.getEnum(jsonMessage.getString("state")), con);
