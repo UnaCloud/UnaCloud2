80ae75aeb4dada63880e88af66d8f703a6a7020f
Juan Pablo Vinchira Salazar <jp.vinchira10@uniandes.edu.co>
2016-07-10 13:19:43 -0500

    Added '/' to FileManager.java from UnaCloudFileManager.

--- UnaCloudFileManager/src/java/uniandes/unacloud/FileManager.java
+++ UnaCloudFileManager/src/java/uniandes/unacloud/FileManager.java
@@ -40,8 +40,8 @@ public class FileManager extends ProjectManager{
 
 	@Override
 	protected String getPropetiesFileName() {	
-		System.out.println("Load file: "+EnvironmentManager.getConfigPath()+UnaCloudConstants.FILE_CONFIG);
-		return EnvironmentManager.getConfigPath()+UnaCloudConstants.FILE_CONFIG;	
+		System.out.println("Load file: "+EnvironmentManager.getConfigPath()+'/'+UnaCloudConstants.FILE_CONFIG);
+		return EnvironmentManager.getConfigPath()+'/'+UnaCloudConstants.FILE_CONFIG;	
 	}
 
 	@Override
