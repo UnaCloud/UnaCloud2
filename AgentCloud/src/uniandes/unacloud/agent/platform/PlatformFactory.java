2b70a168e68961893087d6fc9ac56cb36c2f9984
Sergio Guzmán Mayorga <supersergio.sgm@gmail.com>
2018-03-21 21:07:00 -0500

    Syntaxes error

--- AgentCloud/src/uniandes/unacloud/agent/platform/PlatformFactory.java
+++ AgentCloud/src/uniandes/unacloud/agent/platform/PlatformFactory.java
@@ -61,7 +61,7 @@ public class PlatformFactory {
      * @param platformId The platform id to be instantiated
      * @return A managed platform for the given name
      */
-    public static Platform getPlatform(final String platformId) {´
+    public static Platform getPlatform(final String platformId) {
     	return map.get(platformId);
     }
     
